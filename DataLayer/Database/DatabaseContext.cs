45718d739a8e1c7c4d9ee9ca45ea55eab9a5858f
Venislav Kirilov <leagueoflegendsisez@abv.bg>
2025-02-24 00:04:10 +0200

    Added connection to a SQLite Database. Added 2 tables on creation, "Users" and "Logs". In Users we store data for our users and in Logs we store all logs related to creation/deletion of a user. Added a database logger. Added a simple menu in which we can add/remove/show users.

---
+++ DataLayer/Database/DatabaseContext.cs
@@ -0,0 +1,60 @@
+﻿using DataLayer.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Welcome.Others;
+
+namespace DataLayer.Database
+{
+    internal class DatabaseContext : DbContext
+    {
+        public DbSet<DatabaseUser> Users { get; set; }
+        public DbSet<DBLogEntry> Logs { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string solutionFolder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string databaseFile = "Welcome.db";
+            string databasePath = Path.Combine(solutionFolder, databaseFile);
+            optionsBuilder.UseSqlite($"Data Source={databasePath}");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DatabaseUser>().Property(e => e.Id).ValueGeneratedOnAdd();
+            modelBuilder.Entity<DBLogEntry>().Property(e => e.Id).ValueGeneratedOnAdd();
+
+            var adminUser = new DatabaseUser()
+            {
+                Id = 1,
+                Name = "John Doe",
+                Password = "admin123",
+                Role = UserRolesEnum.ADMIN,
+                Expires = DateTime.Now.AddYears(10)
+            };
+
+            var professorUser = new DatabaseUser()
+            {
+                Id = 2,
+                Name = "Ivan Ivanov",
+                Password = "professor123",
+                Role = UserRolesEnum.PROFESSOR,
+                Expires = DateTime.Now.AddYears(6)
+            };
+
+            var studentUser = new DatabaseUser()
+            {
+                Id = 3,
+                Name = "Georgi Georgiev",
+                Password = "student123",
+                Role = UserRolesEnum.STUDENT,
+                Expires = DateTime.Now.AddYears(4)
+            };
+
+            modelBuilder.Entity<DatabaseUser>()
+                .HasData(adminUser, professorUser, studentUser);
+        }
+    }
+}
---
+++ DataLayer/Model/DBLogEntry.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataLayer.Model
+{
+    public class DBLogEntry
+    {
+        [Key]
+        public int Id { get; set; }
+        public string LogLevel { get; set; }
+        public int EventId { get; set; }
+        public string Message { get; set; }
+        public string? Exception { get; set; }
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+    }
+}
+
