e0224d529c8830b513fd04d002e91322bef6b35e
Venislav Kirilov <leagueoflegendsisez@abv.bg>
2025-02-25 23:40:58 +0200

    Adding UI Project where we display the data from our databases. Passwords are protected by a converter and full log messages are shown by double clicking the row you want the information from.

---
+++ UI/Extras/Converters/HiddenPasswordConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace UI.Extras.Converters
+{
+    public class HiddenPasswordConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string password && !string.IsNullOrEmpty(password))
+            {
+                return new string('*', password.Length);
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // ConvertBack is not needed since we don’t allow editing passwords from UI
+            throw new NotImplementedException();
+        }
+    }
+}
